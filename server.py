aefe7ae7d35dde18968bba6e952d3f78d76c0ca4
FULCOS <42112139+FULCOS@users.noreply.github.com>
2022-11-04 11:13:06 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,48 @@
+
 import socket
 
-port = 53
+ 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# A TCP based echo server
 
-while True:
-    test = s.bind(('', port))
+echoSocket = socket.socket();
 
-    print(test)
+ 
+
+# Bind the IP address and the port number
+
+echoSocket.bind(("127.0.0.1", 53));
+
+ 
+
+# Listen for incoming connections
+
+echoSocket.listen();
+
+ 
+
+# Start accepting client connections
+
+while(True):
+
+    (clientSocket, clientAddress) = echoSocket.accept();
+
+   
+
+    # Handle one request from client
+
+    while(True):
+
+        data = clientSocket.recv(1024);
+
+        print("At Server: %s"%data);
+
+ 
+
+        if(data!=b''):
+
+            # Send back what you received
+
+            clientSocket.send(data);
+
+            break;
